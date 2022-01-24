20ba3c2e67423349e8b78c6416ff93e46098c2a9
OrrEos <eo379@cam.ac.uk>
2022-01-24 15:32:28 +0000

    EO: 1B &1C changes to geo, task1C attempt

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,15 +7,30 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+from floodsystem.stationdata import build_station_list
 
-<<<<<<< HEAD
 
 #task 1B: list of (station, distance) tuples from coord p 
 #sort the list by distance
 
-#required function signature:
-=======
-#testing push pull
-#testing again
-
->>>>>>> cd92c4f2ba46a53cf711d5dbf1a39f0dc03be42b
+#required function signature
+def stations_by_distance(stations, p):
+    #first, a list of all stations
+    stations = build_station_list()
+    ret = []
+    for station in stations:
+        distance_p = haversine(p, station.coord)
+        a = (station.name, distance_p)
+        ret.append(a)
+    return sorted_by_key(ret, 1, reverse=False)
+    
+#task 1C
+def stations_within_radius(stations, centre, r):
+    stations = build_station_list()
+    ret = []
+    for station in stations:
+        distance_x = haversine(centre, station.coord)
+        if distance_x <= r:
+            ret.append(station.name)
+    return sorted(ret)
